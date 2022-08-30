0e03c33bb9fae5d4c76db41c912f61ece530951d
Javier Nava <javier.nava@carvana.com>
2022-08-29 21:35:01 -0500

    Final logo updated

--- src/assets/logo.js
+++ src/assets/logo.js
@@ -2,50 +2,26 @@ import * as React from "react"
 
 const Logo = (props) => (
   <svg
-    height="64px"
+    viewBox="0 0 6084 1667"
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="133.031 9.2 233.938 131.6"
+    xmlSpace="preserve"
     style={{
-      background: "0 0",
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      strokeLinejoin: "round",
+      strokeMiterlimit: 2,
+      viewBox: 20
     }}
-    preserveAspectRatio="xMidYMid"
+    height={'32px'}
     {...props}
   >
-    <defs>
-      <filter id="a">
-        <feGaussianBlur stdDeviation={4} in="SourceAlpha" result="blur" />
-        <feSpecularLighting
-          surfaceScale={5}
-          specularConstant={0.8}
-          specularExponent={7.5}
-          lightingColor="#7aacd1"
-          in="blur"
-          result="specular"
-        >
-          <fePointLight x={-250} y={-50} z={300} />
-        </feSpecularLighting>
-        <feComposite
-          operator="in"
-          in="specular"
-          in2="SourceAlpha"
-          result="comp"
-        />
-        <feComposite
-          in="SourceGraphic"
-          in2="comp"
-          operator="arithmetic"
-          k2={1}
-          k3={1}
-        />
-      </filter>
-    </defs>
-    <g filter="url(#a)">
-      <path
-        d="M176.47 98.03V51.97h7.63l12.37 28.43v11.57h-2L184.1 66.34v31.69h-7.63Zm22.56-17.5 12.38-28.56h7.62v46.06h-7.62V66.34L201.1 91.97h-2.07V80.53Zm39.25-28.56v46.06h-7.62V51.97h7.62Zm37.19 31.75V51.97h7.63v46.06h-7.63l-15.5-27.25V56.4l15.5 27.32Zm-18.06 14.31h-7.63V51.97h7.63v46.06Zm37.31 0V51.97h7.63v46.06h-7.63Zm26.88-25.88v5.13h-16.69v-5.13h16.69Zm-16.69 25.88v-5.69h18.62v5.69h-18.62Zm0-46.06h18.62v5.68h-18.62v-5.68Z"
-        fill="#1d7ed1"
-      />
-    </g>
+    <path
+      d="M0 1666.67V0l833.333 416.667v1250H0Zm1750 0V0L916.667 416.667v1250H1750Zm1166.67 0V0l833.333 416.667v1250H2916.67ZM4666.67 0v1666.67l-833.334-416.667V.003h833.334Zm166.66 1166.67h1250v500h-1250zm0-583.337h1250v500h-1250zm0-583.333h1250v500h-1250zM1916.67 0h833.333v1666.67H1916.67z"
+      style={{
+        fill: "#1290cb",
+      }}
+    />
   </svg>
 )
 
-export default Logo
+export default Logo;
